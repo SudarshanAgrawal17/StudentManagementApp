d589e23004b650a08200a1527936bd53e74de370
SudarshanAgrawal17 <101314880+SudarshanAgrawal17@users.noreply.github.com>
2023-04-17 13:43:47 +0530

    adding functionalities

--- src/App.java
+++ src/App.java
@@ -30,10 +30,36 @@ public class App {
                     }
                     break;
                 case 2:
+                    if(!StudentDao.showAllStudent()){
+                        System.out.println("Something went wrong!");
+                    }
                     break;
                 case 3:
+                    System.out.print("(Write original if you don't want to update the attribute)\nEnter id of the student to that needs to be updated: ");
+                    int id = sc.nextInt();
+                    System.out.print("Enter name to update: ");
+                    sc.nextLine(); // to consume the buffer
+                    name = sc.nextLine();
+                    System.out.print("Enter phone number to update: ");
+                    phone = sc.nextLine();
+                    System.out.print("Enter city to update: ");
+                    city = sc.nextLine();
+                    if(StudentDao.updateStudent(id, name, phone, city)){
+                        System.out.println("Student updated successfully!");
+                    }
+                    else{
+                        System.out.println("Something went wrong!");
+                    }
                     break;
                 case 4:
+                    System.out.print("Enter user id: ");
+                    id = sc.nextInt();
+                    if(StudentDao.deleteStudent(id)){
+                        System.out.println("Student deleted successfully");
+                    }
+                    else{
+                        System.out.println("Something went wrong");
+                    }
                     break;
                 case 5:
                     sc.close();
--- src/StudentDao.java
+++ src/StudentDao.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class StudentDao {
     public static boolean insertStudentToDB(Student st){
@@ -19,6 +20,76 @@ public class StudentDao {
             flag = true;
         }
         catch(Exception e){
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public static boolean deleteStudent(int id) {
+        boolean flag = false;
+        try{
+            Connection con = ConnectDB.createC();
+            String query = "delete from students where sid = ?"; // dynamic query with '?'
+            PreparedStatement ps = con.prepareStatement(query);
+
+            //set the values of parameters
+            ps.setInt(1, id);
+
+            // execute
+            ps.executeUpdate();
+            flag = true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public static boolean showAllStudent() {
+        boolean flag = false;
+        try {
+            Connection con = ConnectDB.createC();
+            String query = "select * from students";
+            java.sql.Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+
+            while(res.next()){
+                // can also pass column name inplace of cloumnIndex
+                int id = res.getInt(1);
+                String name = res.getString(2);
+                String phone = res.getString(3);
+                String city = res.getString(4);
+
+                System.out.println("ID: "+id);
+                System.out.println("Name: "+name);
+                System.out.println("Phone Number: "+phone);
+                System.out.println("City: "+city);
+                System.out.println("----------------------");
+                
+                flag = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public static boolean updateStudent(int id, String name, String phone, String city) {
+        boolean flag = false;
+        try {
+            Connection con = ConnectDB.createC();
+            String query = "update students set sname = ?, sphone = ?, scity = ? where sid = ?";
+            PreparedStatement st = con.prepareStatement(query);
+            
+            st.setString(1, name);
+            st.setString(2, phone);
+            st.setString(3, city);
+            st.setInt(4, id);
+            
+            st.executeUpdate();
+            
+            flag = true;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return flag;
